8c696a387fadc2bae678576ac7e1c05ef62d21cd
arjunak2 <ashokarjunakani@gmail.com>
2023-06-03 13:52:59 -0700

    updating svgs

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,29 +1,21 @@
-// import { ReactComponent as Avengers } from "./avengers.svg";
+import { ReactComponent as Avengers } from "./avengers.svg";
 import { ReactComponent as BlackPanther } from "./black_panther.svg";
 import { ReactComponent as BlackPantherCute } from "./black_panther_cute.svg";
 import { ReactComponent as BlackWidow } from "./black_widow.svg";
 import { ReactComponent as CaptainAmerica } from "./captain_america.svg";
 import { ReactComponent as Cyclops } from "./cyclops.svg";
+import { ReactComponent as Daredevil } from "./daredevil.svg";
 import { ReactComponent as Groot } from "./groot.svg";
 import { ReactComponent as Hulk } from "./hulk.svg";
 import { ReactComponent as IronMan } from "./iron_man.svg";
+import { ReactComponent as Loki } from "./loki.svg";
 import { ReactComponent as Rocket } from "./rocket.svg";
 import { ReactComponent as ScarletWitch } from "./scarlet_witch.svg";
-import { ReactComponent as Shield } from "./shield.svg";
-const SpiderGwen = require("./spider_gwen.svg").ReactComponent;
+import { ReactComponent as SpiderGwen } from "./spider_gwen.svg";
+import { ReactComponent as Thor } from "./thor.svg";
+import { ReactComponent as Wolverine } from "./wolverine.svg";
+import { ReactComponent as WolverineCute } from "./wolverine_cute.svg";
 
-const Avengers = require("./avengers.svg").ReactComponent;
-
-// let BlackPanther = "./black_panther.svg";
-// let BlackWidow = "./black_widow.svg";
-// let CaptainAmerica = "./captain_america.svg";
-// let Cyclops = "./cyclops.svg";
-// let Groot = "./groot.svg";
-// let Hulk = "./hulk.svg";
-// let IronMan = "./iron_man.svg";
-// let Rocket = "./rocket.svg";
-// let ScarletWitch = "./scarlet_witch.svg";
-// let SpiderGwen = "./spider_gwen.svg";
 export const Icons = {
     Avengers,
     BlackPanther,
@@ -31,13 +23,17 @@ export const Icons = {
     BlackWidow,
     CaptainAmerica,
     Cyclops,
+    Daredevil,
     Groot,
     Hulk,
     IronMan,
+    Loki,
     Rocket,
     ScarletWitch,
-    Shield,
     SpiderGwen,
+    Thor,
+    Wolverine,
+    WolverineCute,
 };
 
 export type IconNames = keyof typeof Icons;
